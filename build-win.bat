7546e8fcfb6257a697dc783eb034f0124d072b53
yindf <yindf@mail.jj.cn>
2024-12-16 14:46:38 +0800

    add script to build package

---
+++ build-win.bat
@@ -0,0 +1,4 @@
+dotnet publish -c Release -r win-x64 -o ./build src/SourceGit.csproj
+cd build
+del build.zip
+tar -a -c -f build.zip av_libglesv2.dll libHarfBuzzSharp.dll libonigwrap.dll libSkiaSharp.dll README.md SourceGit.exe SourceGit.pdb
---
+++ upload.bat
@@ -0,0 +1,3 @@
+scp build/build.zip lbl@192.168.16.51:/Users/liboliang/ServerData/sourcegit/
+scp version.json lbl@192.168.16.51:/Users/liboliang/ServerData/sourcegit/
+pause
